c94f6907b3cfea4891787fcbbb7e2e966c640d43
KRom-i <emailroma@mail.ru>
2021-04-22 00:20:13 +0300

    test

--- MyChat/src/Server/AuthSetvice.java
+++ MyChat/src/Server/AuthSetvice.java
@@ -1,4 +1,3 @@
-/*
 package Server;
 
 import java.sql.*;
@@ -8,162 +7,261 @@ import java.util.List;
 public class AuthSetvice {
 
     private static Connection connection;
-    private static Statement statement;
 
     public static void connect(){
 
         try {
             Class.forName("org.sqlite.JDBC");
             connection = DriverManager.getConnection("jdbc:sqlite:chat-db.db");
-            statement = connection.createStatement();
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
 
     }
-    public static boolean addUser(String log, String pass, String nick){
 
-        try {
-            String query = "INSERT INTO users (login, password, nickname, history) VALUES (?, ?, ?, ?);";
-            PreparedStatement ps = connection.prepareStatement(query);
-            ps.setString(1, log);
-            ps.setInt(2, pass.hashCode());
-            ps.setString(3, nick);
-            ps.setString(4, "Check in new user");
-            ps.executeUpdate();
 
-            String queryBl = "INSERT INTO blacklist (nickname, usersBlackList) VALUES (?, ?);";
-            PreparedStatement psBl = connection.prepareStatement(queryBl);
-            psBl.setString(1, nick);
-            psBl.setString(2, "null");
-            psBl.executeUpdate();
+////      Добавление нового пользователя в БД.
+//    public static boolean addUser(String log, String pass, String nick){
+//
+//        PreparedStatement statement = null;
+//
+//        try {
+//
+//            String query = "INSERT INTO users (login, password, nickname) VALUES (?, ?, ?);";
+//            statement = connection.prepareStatement(query);
+//
+//            statement.setString(1, log);
+//            statement.setInt(2, pass.hashCode());
+//            statement.setString(3, nick);
+//            statement.executeUpdate();
+//
+//            return true;
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        } finally {
+//            statementClose(statement);
+//        }
+//        return false;
+//    }
 
-            return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
+//    Обновление blacklist.
     public static List<String> blacklistIni(String nickname){
 
-        List<String> blacklist = new ArrayList<>();
-
-        String query = String.format("select usersBlackList from blacklist where nickname='%s'",nickname);
+        List<String> historyList = new ArrayList<>();
+        PreparedStatement statement = null;
+        ResultSet rs = null;
 
         try {
-            ResultSet rs = statement.executeQuery(query);
 
-            if (!rs.getString(1).equals("null")){
-                if (rs.next()){
-                    String[] tokens = rs.getString(1).split(";");
-                    for (String t: tokens) {
-                        blacklist.add(t);
-                    }
-                }
+            statement = connection.prepareStatement(
+                    "SELECT * FROM blacklist WHERE nickname = ?;"
+            );
+
+            statement.setString(1,nickname);
+
+            rs = statement.executeQuery();
+
+            while (rs.next()){
+                historyList.add(rs.getString("userBlackList"));
             }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
 
-        return blacklist;
-    }
-
-    public static void addUserBlackList(String nickname, List<String> blacklist){
-
-        String bl = "";
-
-        for (int i = 0; i < blacklist.size(); i++) {
-            bl += blacklist.get(i) + ";";
-        }
-
-        String queryEdit = "UPDATE blacklist SET usersBlackList='"+ bl + "'" +
-                " WHERE nickname = '" + nickname + "';";
-
-        try {
-            PreparedStatement psEdit = connection.prepareStatement(queryEdit);
-            psEdit.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            statementClose(statement);
+            resultSetClose(rs);
         }
+
+        return historyList;
     }
 
-    public static boolean checkReg(String strfrom, String strVal){
+
+//    Добавление пользователя в blacklist БД.
+    public static int addUserBlackList(String nickname, String usersBlackList){
+
+        PreparedStatement statement = null;
+
         try {
-        String query = String.format("select "+ strfrom +" from users where " + strfrom + "='%s'", strVal);
-            ResultSet rs = statement.executeQuery(query);
-        if (rs.next()){
-            return true;
+
+            String query = "INSERT INTO blacklist (nickname, userBlackList) VALUES (?, ?);";
+            statement = connection.prepareStatement(query);
+
+            statement.setString(1, nickname);
+            statement.setString(2, usersBlackList);
+
+            return statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            statementClose(statement);
         }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        return false;
+        return 0;
     }
 
+    //    Удаление пользователя из blacklist БД.
+    public static void delUserBlackList(String nickname, String usersBlackList){
+
+        PreparedStatement statement = null;
+
+        try {
+
+            String query = "DELETE from blacklist WHERE nickname = ? AND userBlackList = ?;";
+            statement = connection.prepareStatement(query);
+
+            statement.setString(1, nickname);
+            statement.setString(2, usersBlackList);
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            statementClose(statement);
+        }
+
+    }
+
+//    Проверка на уникальность "nickname" и "login" при регистрации нового пользователя
+    public static boolean checkReg(String strfrom, String strVal){
+
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+
+        try {
+
+            statement = connection.prepareStatement(
+                    "SELECT * FROM users WHERE " + strfrom + " = ?;"
+            );
+
+
+            statement.setString(1,strVal);
+
+            rs = statement.executeQuery();
+
+            if (rs.next()) {
+                return true;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            statementClose(statement);
+            resultSetClose(rs);
+        }
+
+        return false;
+
+    }
+
+//     Записть сообщения в историю БД.
     public static void historyMsgAdd(String nickname, String msg){
 
-        String history = (historyMsg(nickname) + "\n" + msg);
-
-        String queryEdit = "UPDATE users SET history='"+ history + "'" +
-                " WHERE nickname = '" + nickname + "';";
+        PreparedStatement statement = null;
 
         try {
 
-            PreparedStatement psEdit = connection.prepareStatement(queryEdit);
-            psEdit.executeUpdate();
+            String query = "INSERT INTO historyMSG (nickname, history) VALUES (?, ?);";
+            statement = connection.prepareStatement(query);
+
+            statement.setString(1, nickname);
+            statement.setString(2, msg);
+            statement.executeUpdate();
 
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            statementClose(statement);
         }
-
-
     }
 
-    public static String historyMsg(String nickname){
+//    Запрос истории сообщений.
+    public static List<String> historyMsg(String nickname){
 
-        String query = String.format("select history from users where nickname='%s'",nickname);
+        List<String> historyList = new ArrayList<>();
+        PreparedStatement statement = null;
+        ResultSet rs = null;
 
         try {
-            ResultSet rs = statement.executeQuery(query);
 
-            if (rs.next()){
-                return rs.getString(1);
+            statement = connection.prepareStatement(
+                    "SELECT * FROM historyMSG WHERE nickname = ?;"
+            );
+
+            statement.setString(1,nickname);
+
+           rs = statement.executeQuery();
+
+           while (rs.next()){
+               historyList.add(rs.getString("history"));
             }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            statementClose(statement);
+            resultSetClose(rs);
         }
-        return "No history";
+
+        return historyList;
     }
 
+//    Проверка значений login/password (при ошибке авторизации возвращает null).
     public static String getNicknameByLoginAndPassword(String login, String password){
-        String query = String.format("select nickname, password from users where login='%s'",login);
+
+        PreparedStatement statement = null;
+        ResultSet rs = null;
 
         try {
-            ResultSet rs = statement.executeQuery(query);
-            int passHash = password.hashCode();
+
+            statement = connection.prepareStatement(
+                    "SELECT * FROM users WHERE login = ?;"
+            );
+
+            statement.setString(1,login);
+
+            rs = statement.executeQuery();
 
             if (rs.next()){
-                String nick = rs.getString(1);
 
-                int dbHash = rs.getInt(2);
+                int passHash = password.hashCode();
+
+                String nick = rs.getString("nickname");
+                int dbHash = rs.getInt("password");
 
                 if (passHash == dbHash){
                     return nick;
                 }
             }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            statementClose(statement);
+            resultSetClose(rs);
         }
+
         return null;
     }
 
     public static void disconnect(){
         try {
             connection.close();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void statementClose(Statement statement){
+        try {
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void resultSetClose(ResultSet resultSet){
+        try {
+            resultSet.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
-*/
